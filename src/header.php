615ef4fa68c360d961645ca1b80076e444e44bab
luckystar520 <ychen5@live.maryville.edu>
2021-11-21 19:36:13 -0800

    Finish login logout signup frontend

--- src/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <div class="header">
-        <h1>PASTE BOARD</h1>
-        <div id="welcome_user">
-            Hi <span id="username"></span>[<a href="#">logout</a>]
-        </div>
-    </div>
-</header>
